752f7ec9d12c04a89075c9ec4d7f4441f6fc9e89
gali zorea <galizorea@galis-macbook-pro-new.local>
2016-09-23 01:03:30 +0300

    added func + func2 + convert to swift 3

--- swiftProj/ViewController.swift
+++ swiftProj/ViewController.swift
@@ -12,23 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let finalsq = 25
-        var board = [Int](count: finalsq + 1, repeatedValue: 0)
-        board[3] = +08; board[06] = +11; board[09] = +09; board[10] = +02
-        board[14] = -10; board[19] = -11; board[22] = -02; board[24] = -08
-        
-        var square = 0
-        var dice = 0
-        while square < finalsq
-        {
-            dice++
-            if dice == 7 {dice = 0}
-            square += dice
-            if square < board.count
-            {
-                square += board[square]
-            }
-        }
+        game2()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -39,4 +23,63 @@ class ViewController: UIViewController {
 
 
 }
+
+
+func game ()
+{
+    
+    let finalsq = 25
+    var board = [Int](repeating: 0 ,count: finalsq + 1)
+    board[3] = +08; board[06] = +11; board[09] = +09; board[10] = +02
+    board[14] = -10; board[19] = -11; board[22] = -02; board[24] = -08
+    
+    var square = 0
+    var dice = 0
+    while square < finalsq
+    {
+        dice += 1
+        if dice == 7 {dice = 0}
+        square += dice
+        if square < board.count
+        {
+            square += board[square]
+        }
+    }
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+func game2()
+{
+    
+    let finalsq = 25
+    var board = [Int](repeating: 0, count: finalsq + 1)
+    
+    board[3] = +08; board[06] = +11; board[09] = +09; board[10] = +02
+    board[14] = -10; board[19] = -11; board[22] = -02; board[24] = -08
+    
+    var sq = 0
+    var dice = 0
+    
+    gameloop: while ( sq != finalsq)
+    {
+        dice+=1
+        if dice == 7 {dice = 1}
+        
+        switch(sq + dice)
+        {
+        case finalsq:
+            break gameloop
+            
+        case let temp where temp > finalsq:
+            continue gameloop
+            
+        default:
+            sq += dice
+            sq += board[sq]
+        }
+    }
+    
+    print("game over")
+}
+
 
